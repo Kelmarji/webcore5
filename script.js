5bd7d3f122bea9f640e6e6099e558f14d10a7140
Kelmarji <148019339+Kelmarji@users.noreply.github.com>
2023-11-02 22:34:03 +0300

    REBUILD

--- script.js
+++ script.js
@@ -2,21 +2,50 @@ const expander = document.getElementById('expand');
 const itemsTabletHide = document.querySelectorAll('.tablet--hide')
 let expanderText = document.querySelector('.content__expand--text')
 const expanderPic = document.querySelector('.expander__img--rotate')
-console.log(itemsTabletHide)
-console.log(expander)
-console.log(expander.textContent)
+let heightWrapper = document.querySelector('.swiper-container')
+let heightSwiperCont = document.querySelector('.swiper-wrapper')
 
-expander.addEventListener('click', function() {
-    for (let i = 0; i < itemsTabletHide.length;i++) {
-        let item = itemsTabletHide[i]
-        item.classList.toggle('tablet--hide')
-    }
-    if (expanderText.textContent === 'Скрыть') {
-        expanderText.textContent = 'Показать всё';
-    } else {
-        expanderText.textContent = 'Скрыть'; 
-    }
+expander.addEventListener('click', function() { 
+  heightWrapper.classList.toggle('is-open')
+  heightSwiperCont.classList.toggle('is-open')
+  if (expanderText.textContent === 'Скрыть') {
+    expanderText.textContent = 'Показать всё';
+} else {
+    expanderText.textContent = 'Скрыть';
+}
     expanderPic.classList.toggle('expander__img--rotate')
 });
         
+var mySwiper; 
 
+function initializeSwiper() {
+  mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1.1,
+    spaceBetween: 16,
+    slidesPerColumn: 4,
+    autoHeight: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+}
+
+function destroySwiper() {
+  if (mySwiper !== undefined && mySwiper !== null) {
+    mySwiper.destroy();
+  }
+}
+
+
+if (window.innerWidth < 768) {
+  initializeSwiper();
+}
+
+window.addEventListener('resize', function () {
+  if (window.innerWidth < 768) {
+    initializeSwiper();
+  } else {
+    destroySwiper();
+  }
+});
